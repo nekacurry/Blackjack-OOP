de7bcf54d57f4e41e40a642fcf9c9377dd833b6e
nekacurry <anneka498@gmail.com>
2020-12-07 19:28:15 -0600

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,6 +1,20 @@
 from card import Card
+import random
 
 class Deck:
 
-  def __init__()
+  def __init__(self):
+    self.cards = [Card(suit, value) 
+                  for suit in ["Hearts", "Clubs", "Diamonds", "Spades"]
+                  for value in ["A", "2", "3", "4", "5", "6",
+                  "7", "8","9", "10", "J", "Q", "K"]] * 4
+    self.shuffle()
+
+  def shuffle(self):
+
+
+
+    
+
+    
 
