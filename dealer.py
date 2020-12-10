d6c463b3a85859965e58f77a291340fe7d0b9799
nekacurry <anneka498@gmail.com>
2020-12-09 21:33:01 -0600

    dealer now subclass of player

--- dealer.py
+++ dealer.py
@@ -1,33 +1,15 @@
-from hand import Hand
-from deck import Deck
+from player import Player
 
-class Dealer(Hand):
-  def __init__(self, name, deck, bust=False):
-    Hand.__init__(self)
-    self.name = 'Dealer'
-    self.deck = deck
-    self.bust = bust
-
-  def show_hand(self):
-    for card in self.cards:
-      print(card)
-    print
-
-  def stand(self):
-    print(f"{self.name} stands with {self.score}")
-
-  def hit(self):
-    self.draw_card(self.deck.deal())
-    print(f"{self.name} hits")
-    return self.cards
-
-  def check_for_bust(self):
-    if self.calculate_score() > 21:
-      self.bust = True
-      print(f"{self.name} gets bust")
-    else:
-      self.stand()
+class Dealer(Player):
+  def __init__(self):
+    super().__init__()
+    self.cards = []
+    self.value = 0
 
   def display(self):
-    print(self.cards)
+    print("hidden")
+    print(self.cards[1])
+    
+
+  
 
