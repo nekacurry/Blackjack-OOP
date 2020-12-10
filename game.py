038a41a8bd2f3242e95703d4c2a77ca6df65d775
nekacurry <anneka498@gmail.com>
2020-12-09 21:47:20 -0600

    Update game.py

--- game.py
+++ game.py
@@ -28,49 +28,49 @@ class Game:
       print("Dealer's hand is:")
       self.dealer_hand.display()
 
-    game_over = False
+      game_over = False
 
-    while not game_over:
-      player_21, dealer_21 = self.check_21()
-      if player_21 or dealer_21:
-        game_over = True
-        self.results(
-            player_21, dealer_21)
-        continue
-    
-      choice = input(("Please choose [Hit / Stand] ").lower())
-      while choice not in ["h", "s", "hit", "stand"]:
-        choice = input("Please enter 'hit' or 'stand' (or H/S) ").lower()
+      while not game_over:
+        player_21, dealer_21 = self.check_21()
+        if player_21 or dealer_21:
+          game_over = True
+          self.results(
+              player_21, dealer_21)
+          continue
+      
+        choice = input(("Please choose [Hit / Stand] ").lower())
+        while choice not in ["h", "s", "hit", "stand"]:
+          choice = input("Please enter 'hit' or 'stand' (or H/S) ").lower()
 
-        if choice in ['hit', 'h']:
-          self.player_hand.add_card(self.deck.deal())
-          self.player_hand.display()
-          if self.player_is_over():
-            print("You busted! You lose!")
+          if choice in ['hit', 'h']:
+            self.player_hand.add_card(self.deck.deal())
+            self.player_hand.display()
+            if self.player_is_over():
+              print("You busted! You lose!")
+              game_over = True
+
+          else:
+            player_hand = self.player_hand.get_value()
+            dealer_hand = self.dealer_hand.get_value()
+
+            print("~Results~")
+            print("Your hand:", player_hand)
+            print("Dealer's hand:", dealer_hand)
+
+            if player_hand > dealer_hand:
+              print("You Win!")
+            elif player_hand == dealer_hand:
+              print("It's a Tie!")
+            else:
+              print("Dealer Wins!")
             game_over = True
 
+        again = input("Play Again? [Y/N] ")
+        if again.lower() == "n":
+          print("Thanks for playing!")
+          playing = False
         else:
-          player_hand = self.player_hand.get_value()
-          dealer_hand = self.dealer_hand.get_value()
-
-          print("~Results~")
-          print("Your hand:", player_hand)
-          print("Dealer's hand:", dealer_hand)
-
-          if player_hand > dealer_hand:
-            print("You Win!")
-          elif player_hand == dealer_hand:
-            print("It's a Tie!")
-          else:
-            print("Dealer Wins!")
-          game_over = True
-
-      again = input("Play Again? [Y/N] ")
-      if again.lower() == "n":
-        print("Thanks for playing!")
-        playing = False
-      else:
-        game_over = False
+          game_over = False
 
   def check_21(self):
     player = False
