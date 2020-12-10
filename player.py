58036452976b4aaf2fd42cc5175e6992d224544b
nekacurry <anneka498@gmail.com>
2020-12-09 21:32:26 -0600

    remade into superclass/merged with previous hand class

--- player.py
+++ player.py
@@ -1,14 +1,34 @@
-from dealer import Dealer
-from hand import Hand
+from card import Card
 
-class Player(Dealer):
-  def __init__(self, name, deck, bust):
-    super().__init__(deck, bust)
-    self.name = name
-    self.deck = deck
-    self.bust = bust
+class Player():
+  def __init__(self):
+    self.cards = []
+    self.value = 0
+
+  def add_card(self, card):
+    self.cards.append(card)
+
+  def calculate_value(self):
+    self.value = 0
+    has_ace = False
+    for card in self.cards:
+      if card.value.isnumeric():
+        self.value += int(card.value)
+      else:
+        if card.value == "A":
+          has_ace = True
+          self.value += 11
+        else:
+          self.value += 10
+
+    if has_ace and self.value > 21:
+      self.value -= 10
+
+  def get_value(self):
+    self.calculate_value()
+    return self.value   
 
   def display(self):
     for card in self.cards:
       print(card)
-    print(f"Your Score: {self.get_score}")
+    print("Value:", self.get_value())
